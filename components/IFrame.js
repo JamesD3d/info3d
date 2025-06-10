ecb302ac940953033be149b2bce7e4837286a391
Derrick James <derrickjames@gmail.com>
2025-06-10 14:05:07 +0000

    tst

--- components/IFrame.js
+++ components/IFrame.js
@@ -5,7 +5,11 @@ const IFrame = ({ src, width, height, title }) => {
       width={width}
       height={height}
       title={title}
-      style={{ border: 'none' }}
+      style={{ 
+        border: 'none' ,
+        backgroundColor: 'transparent',
+      }}
+      allowTransparency="true"
     />
   );
 };
